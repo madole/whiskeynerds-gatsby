328b728323cc044ca99b493a2f64f8489d4a1d1d
Andrew McDowell <madoliole@gmail.com>
2019-10-09 22:12:43 +1100

    Input -> input

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, createRef } from 'react';
 import { InstantSearch, Index, Hits, connectStateResults } from 'react-instantsearch-dom';
 import algoliasearch from 'algoliasearch/lite';
 import { Root, HitsWrapper, PoweredBy } from './styles';
-import Input from './Input';
+import Input from './input';
 import * as hitComps from './hitComps';
 import styled from 'styled-components';
 
